45a10c38cf4c9d93d5d7846944e34de66b3a3b3c
DmitryAlex2012 <tpmfd27@gmail.com>
2020-10-07 16:43:55 +0300

    Edit mail form. Need delete 'order' from SESSION (it deletes from second time).

--- models/FeedbackForm.php
+++ models/FeedbackForm.php
@@ -32,6 +32,8 @@ class FeedbackForm extends Model
             [['name', 'email', 'phone'], 'required', 'message'=>'Не заполнено поле'],
             // email has to be a valid email address
             ['email', 'email', 'message'=>'Некорректный e-mail'],
+            // safe unimportant fields
+            [['age', 'sex', 'reason', 'receiptDate'], 'safe'],
             // verifyCode needs to be entered correctly
 //            ['verifyCode', 'captcha'],
             [['reCaptcha'], ReCaptchaValidator::className(), 'secret' => '6LeNwsEZAAAAAM21dhz5in7PJkxz_ut8Ojay4W0W', 'uncheckedMessage' => 'Please confirm that you are not a bot.'],
@@ -67,15 +69,22 @@ class FeedbackForm extends Model
 
             $order = $session->get('order');
 
-            $messageContent = "Доктор: " . $order['doctor'] . "\r\n";
-            $messageContent = $messageContent . "Услуга: " . $order['method'] . "\r\n";
+            $messageContent = "Запись на прием к доктору: " . $order['doctor'] . "\r\n";
+            $messageContent = $messageContent . "Услуга: " . $order['method'] . "\n" . "\r\n";
+            $messageContent = $messageContent . "Данные о Заказчике:" . "\r\n";
+            $messageContent = $messageContent . "Телефон: " . $this->phone . "\r\n";
+            $messageContent = $messageContent . "Телефон: " . $this->email . "\r\n";
+            $messageContent = $messageContent . "Возраст: " . $this->age . "\r\n";
+            $messageContent = $messageContent . "Пол: " . $this->sex . "\r\n";
+            $messageContent = $messageContent . "Желаемая дата приема: " . $this->receiptDate . "\r\n";
+            $messageContent = $messageContent . "Причина обращения: " . $this->reason . "\r\n";
+
         }
 
-        $messageContent = $messageContent . "Сообщеие заказчика: " . $this->sex;
-//        . "\r\n"
+        unset($_SESSION['order']);
 
         $session->close();
-        Yii::$app->session->destroy();
+//        Yii::$app->session->destroy();
 
         if ($this->validate()) {
             Yii::$app->mailer->compose()
